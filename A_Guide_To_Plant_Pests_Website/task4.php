a84acc182c96bd2232bb8c89f5367ec752b1bdea
alisateterieva <164929222+alisateterieva@users.noreply.github.com>
2024-03-25 22:14:55 +0200

    Add files via upload

---
+++ A_Guide_To_Plant_Pests_Website/task4.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  Оберіть файл:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Завантажити зображення" name="submit">
+</form>
+
+</body>
+</html>
